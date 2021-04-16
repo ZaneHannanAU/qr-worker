3cad47aad3f05559c9baebaf1d08d55db0e1bbd2
Zane Hannan <zanehannanau@gmail.com>
2021-04-16 14:37:11 +1000

    change to tabs, add optional length max

--- worker/worker.js
+++ worker/worker.js
@@ -1,13 +1,23 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
+	event.respondWith(handleRequest(event.request));
 });
+let D_MAXLEN = 127;
+let MAXLEN = D_MAXLEN;
+if (typeof QR_CODE !== "undefined") {
+	MAXLEN = QR_CODE.get('max_len', {type: "json", cacheTtl: 3600});
+}
 
-async function handleRequest(request) {
-    const { handle_request } = wasm_bindgen;
-    await wasm_bindgen(wasm);
-    const output = handle_request(request.url);
+const stat = n => fetch(`https://http.cat/${n}.jpg`).then(r => new Response(r, {status: n}));
+async function handleRequest(req) {
+	let max_len = (await MAXLEN) ?? D_MAXLEN;
+	if (req.url.length > max_len) {
+		return stat(413);
+	}
+	const { handle_request } = wasm_bindgen;
+	await wasm_bindgen(wasm);
+	const output = handle_request(request.url);
 
-    let res = new Response(output, { status: 200 });
-    res.headers.set('Content-type', 'image/svg+xml');
-    return res;
+	let res = new Response(output, { status: 200 });
+	res.headers.set('Content-type', 'image/svg+xml');
+	return res;
 }
